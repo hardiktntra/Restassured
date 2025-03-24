791b1ef971bc7363e1c8e8d77b0624ca63a69c89
tntra <tntra@Hardik-33H6983>
2025-03-24 11:45:28 +0530

    New code

---
+++ restassured/src/main/java/restassured/apitest.java
@@ -0,0 +1,40 @@
+package restassured;
+
+import org.json.JSONObject;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class apitest {
+    
+    public static void main(String[] args) {
+        RestAssured.baseURI = "https://reqres.in/";
+        String strURITokenForPost = "api/users";
+        
+        RequestSpecification request = RestAssured.given();
+        
+        // Create JSON payload
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "morpheus");
+        requestParams.put("job", "leader");
+        
+        // Convert JSONObject to String
+        request.body(requestParams.toString());
+        
+        // Set headers
+        request.header("Content-Type", "application/json");
+        
+        // Send POST request
+        Response response = request.post(strURITokenForPost);
+        String id = response.jsonPath().getString("id");
+        String createdAt = response.jsonPath().getString("createdAt");
+        
+        System.out.println("Extracted ID: " + id);
+        System.out.println("Created At: " + createdAt);
+
+        
+        // Print response
+        System.out.println("\n Status Code: " + response.getStatusCode());
+        System.out.println("---> Response JSON Body: " + response.getBody().asString());
+    }
+}
